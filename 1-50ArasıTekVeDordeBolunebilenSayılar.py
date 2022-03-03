707eaa94c27fff0854a9c813be48da87b15f0a0b
Nur Sena Günay <90158003+nurgunay@users.noreply.github.com>
2022-03-03 17:44:46 +0300

    Add files via upload

---
+++ 1-50ArasıTekVeDordeBolunebilenSayılar.py
@@ -0,0 +1,7 @@
+# 1 ile 50 arası tek sayılar ve 4' e bölünebilen sayıları yazdıran program 
+
+
+for i in range(1,51):
+    if (i % 2 != 0 or i % 4 == 0 ):
+       print("1-50 Arasındaki Tek ve 4'e Bölünebilen Sayılar: ",i)
+
