0c92d82c70c4f989c017b86624cdf6e2b54da27b
Nur Sena Günay <90158003+nurgunay@users.noreply.github.com>
2022-03-03 16:54:52 +0300

    Add files via upload

---
+++ mutlakDeger.py
@@ -0,0 +1,13 @@
+# girilen sayının mutlak değerini bulan program
+
+sayı = int(input("Mutlak değerini bulmak istediğiniz sayıyı giriniz:"))
+
+if (sayı > 0):
+    print(sayı,"sayısının mutlak değeri: ",sayı)
+   
+elif (sayı == 0):
+    print(sayı,"sayısının mutlak değeri: ",sayı)
+    
+else:
+    print(sayı,"sayısnın mutlak değeri: ",(sayı*(-1)))
+    
